bdc28f40608c0348dfc425259b717417ec3c55e9
abanobmossad <abanobmossad@gmail.com>
2019-08-18 04:53:00 +0200

    Update docker compose file

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const {Client} = require('pg');
+const express = require("express");
+const { Client } = require("pg");
 const server = express();
-const PORT = 3000;
 
 const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  password: '8520',
-  database: 'docker',
-  port: 5432,
-})
+	host: "db",
+	user: "postgres",
+	password: "8520",
+	database: "docker",
+	port: 5432
+});
 
-client.connect().then(()=>{
-  console.log("Connected to DB");
-}).catch(err=>{
-  console.error('Not connected', err);
-})
+client
+	.connect()
+	.then(() => {
+		console.log("Connected to DB");
+	})
+	.catch(err => {
+		console.error("DB Not connected", err);
+	});
 
+const PORT = 3000;
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
-server.get('/', (req, res) => res.status(200).send('Server on docker'));
+server.get("/ping", (req, res) => res.status(200).send("pang"));
